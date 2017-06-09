6152c23a2baaae947b746d361ac2ea8f9486aa3d
Rao InfoTech <raoinfotech@Raos-Mac-mini-2.local>
2017-06-09 16:12:41 +0530

    server deleted

--- server/scan.php
+++
@@ -1,9 +0,0 @@
-<?php
-$ara = scandir('images');
-$data = array();
-for ($i=2; $i < count($ara) ; $i++) {
-	
-	$data[] = array('path' => "http://132.140.160.119/yash_shukla/project_2/server/images/".(str_replace(' ', '',$ara[$i])),
-		'caption' => str_replace("_", " ", substr($ara[$i], 0, -4)));
-}	
-echo json_encode($data);
